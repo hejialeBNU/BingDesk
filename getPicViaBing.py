57731bbc3a3de54db7d1327ec9190a2cec58284e
hejialeBNU <hejialeBNU@163.com>
2017-11-02 23:40:34 +0800

    Initial commit

---
+++ getPicViaBing.py
@@ -0,0 +1,42 @@
+import urllib2
+import urllib
+import datetime
+try:
+    import xml.etree.cElementTree as ET
+except:
+    import xml.etree.ElementTree as ET
+
+today = datetime.datetime.now().strftime('%Y%m%d')
+bg_size = "1920x1080"
+local_path = "E:/background_imgs/"
+bingUrl = "http://www.bing.com"
+#get xml data from bing.com XML
+response = urllib2.urlopen("http://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1")
+#content_type = response.info()["Content-Type"].split(";")[1].split("=")[1]
+xml = response.read()
+response.close()
+
+#Formate XML
+treeRoot = ET.fromstring(xml)
+#formate url and local paths
+urlBase =  bingUrl + treeRoot.find("image").find("urlBase").text
+nameBase = urlBase.split("/")[-1]
+local_name = local_path + today + "_" + nameBase.split("_")[0] + "_" + bg_size + ".jpg"
+bg_url = urlBase + "_" + bg_size + ".jpg"
+urllib.urlretrieve(bg_url, local_name)
+
+
+
+
+
+
+
+
+
+
+
+
+#Decode the Html data that just downloaded
+
+
+
